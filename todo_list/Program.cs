1b467a7c4c843d3ea7a57b490183e030816cd4d1
Jose Reyes <74410806+Khakipapi@users.noreply.github.com>
2024-08-23 08:33:59 -0400

    Add project files.

---
+++ todo_list/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("[S]ee all TODO");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userInput = "A";
+Console.WriteLine(userInput);
+
+int number = 2;
+int a =1 , b = 2;
+string name, lastname;
+
+
+Console.ReadKey();
+
